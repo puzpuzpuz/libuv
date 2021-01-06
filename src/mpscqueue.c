32cdc1cd85992da50a1bbbfe9eea6431c8746465
Andrey Pechkurov <apechkurov@gmail.com>
2021-01-06 22:35:34 +0300

    compiling non-intrusive queue

--- src/mpscqueue.c
+++ src/mpscqueue.c
@@ -15,33 +15,55 @@ void mpscq_create(mpscq_t* self) {
 
 void mpscq_push(mpscq_t* self, mpscq_node_t* n) {
   n->next = NULL;
-  mpscq_node_t* prev = atomic_exchange(&self->head, n);
-  // (*)
+  // serialization-point wrt producers, acquire-release
+  mpscq_node_t* prev = atomic_exchange_explicit(
+      &self->head, n, memory_order_acq_rel);
+  // serialization-point wrt consumer, release
   prev->next = n;
 }
 
 mpscq_node_t* mpscq_pop(mpscq_t* self) {
   mpscq_node_t* tail = self->tail;
+  // serialization-point wrt producers, acquire
   mpscq_node_t* next = tail->next;
-  if (tail == &self->stub) {
-    if (NULL == next)
-      return NULL;
-    self->tail = next;
-    tail = next;
-    next = next->next;
-  }
   if (next) {
     self->tail = next;
-    return tail;
-  }
-  mpscq_node_t* head = self->head;
-  if (tail != head)
-    return NULL;
-  mpscq_push(self, &self->stub);
-  next = tail->next;
-  if (next) {
-    self->tail = next;
+    tail->state = next->state;
     return tail;
   }
   return NULL;
 }
+
+// void mpscq_push(mpscq_t* self, mpscq_node_t* n) {
+//   n->next = NULL;
+//   mpscq_node_t* prev = atomic_exchange_explicit(
+//       &self->head, n, memory_order_acq_rel);
+//   // (*)
+//   prev->next = n;
+// }
+
+// mpscq_node_t* mpscq_pop(mpscq_t* self) {
+//   mpscq_node_t* tail = self->tail;
+//   mpscq_node_t* next = tail->next;
+//   if (tail == &self->stub) {
+//     if (NULL == next)
+//       return NULL;
+//     self->tail = next;
+//     tail = next;
+//     next = next->next;
+//   }
+//   if (next) {
+//     self->tail = next;
+//     return tail;
+//   }
+//   mpscq_node_t* head = self->head;
+//   if (tail != head)
+//     return NULL;
+//   mpscq_push(self, &self->stub);
+//   next = tail->next;
+//   if (next) {
+//     self->tail = next;
+//     return tail;
+//   }
+//   return NULL;
+// }
